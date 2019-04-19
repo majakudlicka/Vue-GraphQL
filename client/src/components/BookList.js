dd6957ae584d6953c5e9e8e20f5a4a021462286a
Maja Kudlicka <maja.kudlicka@gmail.com>
2019-04-19 18:59:40 +0200

    feat: connected React frontEnd to GraphQL using apollo

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getBooksQuery = gql`
+  {
+    books {
+        name
+        id
+    }    
+  }
+`
 
 class BookList extends Component {
+    displayBooks() {
+        var data = this.props.data;
+        if (data.loading) {
+            return(<div>Loading books...</div>)
+        } else {
+            return data.books.map(book => {
+                return (<li key={book.id}>{book.name}</li>)
+            })
+        }
+    }
     render() {
         return (
             <div>
                 <ul id="book-list">
-                    <li>BOOK NAME</li>
+                    {this.displayBooks()}
                 </ul>
 
             </div>
@@ -13,4 +34,4 @@ class BookList extends Component {
     }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// allow cors requests
+app.use(cors());
 
 mongoose.connect('mongodb+srv://shaun:123@cluster0-jc8ym.mongodb.net/test?retryWrites=true');
 mongoose.connection.once('open', ()=> {
